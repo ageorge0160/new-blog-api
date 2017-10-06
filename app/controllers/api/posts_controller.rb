e2ad151560e2621feba57f5b9e8eb02348ee69a9
Amanda George <github email address>
2017-10-05 20:17:58 -0500

    trying to get database to work

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,45 @@
+class Api::PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :destroy]
+     def index
+       @posts = Post.all
+       render json: @posts
+     end
+
+     def create
+       post = Post.new(post_params)
+       if post.save
+         render json: post
+       else
+         render json:{message: post.errors}, status: 400
+       end
+     end
+
+     def show
+       render json: @post
+     end
+
+     def update
+       if @post.update(post_params)
+         render json: @post
+       else
+         render json:{message: post.errors}, status: 400
+       end
+     end
+
+     def destroy
+       if @post.destroy
+         render status: 204
+       else
+         render json:{message: "didn't work"}, status: 400
+       end
+     end
+
+     private
+     def set_post
+       @post = Post.find_by(id: params[:id])
+     end
+
+     def post_params
+       params.require(:post).permit(:title, :content, :created_by)
+     end
+   end
